e6f65371413c558001bd96860bfb432f1b640720
Szilard Ficsor <fszili0514@gmail.com>
2024-02-13 14:26:57 +0100

    Rocket Updates

--- Unwrap Gamejam/Assets/Scripts/PlayerBehaviour.cs
+++ Unwrap Gamejam/Assets/Scripts/PlayerBehaviour.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class PlayerBehaviour : MonoBehaviour
 {
     [SerializeField] public float Fuel;
-    [SerializeField] private float Speed = 50f;
+    [SerializeField] private float Speed = 100f;
     private Rigidbody _rb;
     private Vector3 _moveDirection;
+    private Quaternion _moveRotation;
     private float _rotationX;
     private float _rotationY;
+    private float _elapsedTime;
 
     private void Awake()
     {
@@ -24,7 +26,13 @@ public class PlayerBehaviour : MonoBehaviour
         Vector3 direction = transform.forward;
         _moveDirection = Vector3.Normalize(direction) * Speed;
         _rb.AddForce(_moveDirection, ForceMode.Impulse);
+        //_rb.velocity = transform.forward * Speed;
         GatherInput();
+        MoveRocket();
+    }
+    private void FixedUpdate()
+    {
+        _elapsedTime = Time.fixedDeltaTime;
     }
     private void GatherInput()
     {
@@ -32,8 +40,21 @@ public class PlayerBehaviour : MonoBehaviour
         float moveY = Input.GetAxis("Mouse Y") * 2;
         _rotationX += moveX;
         _rotationY += moveY;
-        _rb.rotation *= Quaternion.Euler(moveY, moveX, 0);
-        transform.rotation *= Quaternion.Euler(moveY,moveX, 0);
+    }
+    private void MoveRocket()
+    {
+        //float percentage = _elapsedTime;
+        //_rb.rotation *= Quaternion.Euler(moveY, moveX, 0);
+        //Quaternion toRot = Quaternion.Euler(_rotationY, _rotationX, -_rotationX);
+        //transform.rotation *= Quaternion.Euler(moveY,moveX, 0);
+        //_moveRotation = Quaternion.Lerp(transform.rotation, toRot, percentage);
+        //transform.localRotation = _moveRotation;
+        //Debug.Log(percentage);
+
+        //float rotX = Input.GetAxisRaw("Horizontal") * 0.25f;
+        //transform.localRotation *= Quaternion.Euler(0, 0, -rotX);
+        var rotation = Quaternion.LookRotation(new Vector3(_rotationY, _rotationX, 0));
+        _rb.MoveRotation(Quaternion.RotateTowards(transform.rotation, rotation, 3 * Time.deltaTime));
 
     }
 }
