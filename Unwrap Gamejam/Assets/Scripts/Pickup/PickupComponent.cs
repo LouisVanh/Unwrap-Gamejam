675e10b4d7fed93b34c4c095c80a2342c559aec3
ralfvn <104133171+RalfVanNieuwenhuyse@users.noreply.github.com>
2024-02-13 12:31:46 +0100

    added pickups

---
+++ Unwrap Gamejam/Assets/Scripts/Pickup/PickupComponent.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static PickupComponent;
+
+public enum PickupType
+{
+    Fuel,
+    Payload
+}
+
+public class PickupComponent : MonoBehaviour
+{
+    [SerializeField] private float _value = 10f;
+    [SerializeField] private PickupType _type;
+
+    private const string FRIENDLY_TAG = "Player";
+
+
+    public delegate void AddFuel(float value);
+    public static event AddFuel OnAddFuel;
+
+    public delegate void AddPayload(float value);
+    public static event AddPayload OnAddPayload;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag(FRIENDLY_TAG)) return;
+
+        switch (_type)
+        {
+            case PickupType.Fuel:
+                OnAddFuel?.Invoke(_value);
+                break;
+            case PickupType.Payload:
+                OnAddPayload?.Invoke(_value);
+                break;
+            default:
+                break;
+        }
+
+        Destroy(gameObject);
+    }
+}
