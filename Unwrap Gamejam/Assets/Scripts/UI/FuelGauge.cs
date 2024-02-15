49b63c37bbe6d9e97572fdf8ab8d02cbd055b4aa
ralfvn <104133171+RalfVanNieuwenhuyse@users.noreply.github.com>
2024-02-15 10:41:09 +0100

    added red flashing fuel gauge

--- Unwrap Gamejam/Assets/Scripts/UI/FuelGauge.cs
+++ Unwrap Gamejam/Assets/Scripts/UI/FuelGauge.cs
@@ -1,10 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FuelGauge : MonoBehaviour
 {
     [SerializeField] private RectTransform pointer;
+
+
+    [SerializeField] private float _minFuel = 10f;
+    [SerializeField] private float _flashDelay = 0.1f;
+    [SerializeField] private Image _pointerImage;
+    [SerializeField] private Image _backgroundImage;
+
+    private bool _doOnce = true;
+
+    private float _fuelAmmount;
 
     private void OnEnable()
     {
@@ -18,6 +29,7 @@ public class FuelGauge : MonoBehaviour
 
     void UpdatePointer(float fuelammount)
     {
+        _fuelAmmount = fuelammount;
         // Clamp the percentage between 0 and 100
         fuelammount = Mathf.Clamp(fuelammount, 0f, 100f);
 
@@ -25,5 +37,38 @@ public class FuelGauge : MonoBehaviour
 
         // Apply rotation to the image transform
         pointer.rotation = Quaternion.Euler(0f, 0f, angle);
+
+        if(fuelammount <= _minFuel && _doOnce) 
+        {
+            StartCoroutine(FlashFuel());
+
+            _doOnce = false;
+        }
+    }
+
+    private IEnumerator FlashFuel()
+    {
+        Color startColor = _pointerImage.color;
+        bool flipFlop = false;
+
+        while (_fuelAmmount <= _minFuel) 
+        {
+            flipFlop = !flipFlop;
+
+            if(flipFlop )
+            {
+                _pointerImage.color = startColor;
+                _backgroundImage.color = startColor;
+            }
+            else
+            {
+                _pointerImage.color = Color.red;
+                _backgroundImage.color = Color.red;
+            }           
+
+            yield return new WaitForSeconds(_flashDelay);
+        }
+
+        _doOnce = true;
     }
 }
